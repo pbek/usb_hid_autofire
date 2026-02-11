5a1e2e888e8dbb765dd230e3be1ee6eb677aaade
Patrizio Bekerle <patrizio@bekerle.com>
2026-02-11 21:00:37 +0100

    devenv: enable clang-format
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- devenv.nix
+++ devenv.nix
@@ -5,5 +5,11 @@ _:
     echo "🛠️ usb_hid_autofire dev shell"
   '';
 
+  git-hooks.hooks = {
+    clang-format = {
+      enable = true;
+    };
+  };
+
   # See full reference at https://devenv.sh/reference/options/
 }
